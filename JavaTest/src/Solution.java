d653e16cb2fcf85ae4814470e3d320a2c16c7693
yeoeunjoo <graceyeo7749@naver.com>
2020-06-30 09:42:15 +0900

    문자열 압축

--- JavaTest/src/Solution.java
+++ JavaTest/src/Solution.java
@@ -2,43 +2,48 @@
 // null인 밸류 탐색하여 키값 출력하려고 했으나 이중for문사용과 논리적인 오류가 있어보였음
 //2차 : 모범답안 참고함
 
-import java.util.Arrays;
-import java.util.Comparator;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] numbers = {6,10,2};
-        Solution s = new Solution();
+        String s = "aabbaccc";
+        Solution sol = new Solution();
 
-        String result = s.solution(numbers);
+        int result = sol.solution(s);
         System.out.println(result);
     }
 
 
 
-    public String solution(int[] numbers) {
-        String answer = "";
-        String[] arr = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
+    public int solution(String s) {
+        int answer = 1001;
 
-            arr[i] = (String.valueOf(numbers[i]));
-        }
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return (s2+s1).compareTo(s1+s2);
+        if(s.length()==1) return 1;
+
+        for(int i=1;i<=s.length()/2;i++){
+            String now, next = "", result = "";
+            int hit = 1;
+            for(int j=0;j<=s.length()/i;j++){
+                int start = j*i;
+                int end = i*(j+1)>s.length()?s.length():i*(j+1);
+                now = next;
+                next = s.substring(start,end);
+                if(now.equals(next)){
+                    hit++;
+                }else{ //같지않으면
+                    result += (continued(hit)+now); //문자열 붙임
+                    hit=1;
+                }
             }
-        });
-
-        //0000 처럼 0으로만 구성되어있으면 0 return
-        if (arr[0].equals("0")) return "0";
-
-        //그 외의 경우 순차적으로 연결하여 answer return
-        for (int i = 0; i < arr.length; i++) {
-            answer+=arr[i];
+            result += (continued(hit)+next);
+            answer = Math.min(answer, result.length());
         }
         return answer;
 
+    }
+
+    //1이면 숫자가 보이지 않게 처리 필요?
+    private static String continued(int hit){
+        return hit>1 ? String.valueOf(hit):"";
     }
 
 }
